5deeed581ae6546a199f7104aa941c0dffc903c8
Bart <bartek@redly.pl>
2023-10-20 05:35:44 +0200

    repaired all issues related to SSR problems, mainly 'window' variable used inside theme detector function

--- src/styles/themeDetector.js
+++ src/styles/themeDetector.js
@@ -1,9 +1,19 @@
 import { useEffect, useState } from "react"
+import useIsSsr from "../utils/useIsSsr"
 
 const useThemeDetector = () => {
-    const getCurrentTheme = () =>
-        window.matchMedia("(prefers-color-scheme: dark)").matches
+    const isSsr = useIsSsr()
+
+    const getCurrentTheme = () => {
+        if (isSsr) {
+            return window.matchMedia("(prefers-color-scheme: dark)").matches
+        } else {
+            return null
+        }
+    }
+
     const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme())
+
     const mqListener = (e) => {
         setIsDarkTheme(e.matches)
     }
